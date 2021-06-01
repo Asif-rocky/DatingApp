6cbecbf22d98f62ed2ddafd3f40f2018ded76906
Asif <asif13199615@gmail.com>
2021-06-01 16:04:43 +0530

    End of section 8

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime Dob)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - Dob.Year;
+            if(Dob.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
