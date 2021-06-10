4de1e8921d86f3cc7d98b8596ddde2c35d69d7fb
Asif <asif13199615@gmail.com>
2021-06-10 16:44:45 +0530

    End  of section9

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,47 +1,48 @@
+import { environment } from './../../environments/environment';
 import { User } from './../_models/user';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { ReplaySubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
-      map((response: User) =>{
+      map((response: User) => {
         const user = response;
-        if(user){
-          localStorage.setItem('user',JSON.stringify(user));
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
       })
     );
   }
 
-  register(model:any){
+  register(model: any) {
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
-      map((user: User) =>{
-        if(user){
-           localStorage.setItem('user',JSON.stringify(user));
-           this.currentUserSource.next(user);
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
         }
       })
-    )
+    );
   }
 
-  setCurrentUser(user: User){
+  setCurrentUser(user: User) {
     this.currentUserSource.next(user);
   }
 
-  logOut(){
+  logOut() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
   }
